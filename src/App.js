527ac09632351003ff3a4c72dcbdb9acb6d9d7b4
JoseIgnacioTapia <josignaciot@gmail.com>
2021-06-28 19:58:34 -0300

    CRUD API: HTTP requests to the REAL API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import CrudApp from './components/CrudApp';
 import CrudApi from './components/CrudApi';
+import SongSearch from './components/SongSearch';
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <SongSearch />
       <hr />
       <CrudApi />
       <hr />

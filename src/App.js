cf64d666349f4e673289fc196c720e5b8454fc34
jose ignacio <josignaciot@gmail.com>
2021-07-03 19:11:12 -0300

    Form Validation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import CrudApp from './components/CrudApp';
 import CrudApi from './components/CrudApi';
 import SongSearch from './components/SongSearch';
 import SelectAnidados from './components/SelectsAnidados';
+import ContactForm from './components/ContactForm';
 
 function App() {
   return (
     <>
       <h1>Ejercicios con React</h1>
+      <hr />
+      <ContactForm />
       <hr />
       <SelectAnidados />
       <hr />
